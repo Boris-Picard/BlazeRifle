e1e0931ad7cde95b827baa39b124684291195fb7
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 <picard.boris@gmail.com>
2023-10-30 16:45:10 +0100

    maj navbar

---
+++ public/assets/js/script.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector(".navbar");
+
+
+/*============= fonction changement de background color navbar =============*/ 
+const scrollNavbar = () => {
+    if(window.scrollY >= 75) {
+        navbar.classList.add("navbarScroll")
+        navbar.classList.remove("navbarStyle")
+    } else {
+        navbar.classList.add("navbarStyle")
+        navbar.classList.remove("navbarScroll")
+    }
+}
+
+window.addEventListener("scroll", scrollNavbar)
