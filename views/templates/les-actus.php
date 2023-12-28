fd2c879e96a1a16892758079fd20d22c78757478
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 <picard.boris@gmail.com>
2023-12-28 21:48:53 +0100

    page des actualités progress, right col et card done

--- views/templates/les-actus.php
+++ views/templates/les-actus.php
@@ -3,10 +3,100 @@
         <div class="container">
             <section>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-8">
                         <div class="row">
-                            <div class="col-12 py-3 justify-content-center d-flex flex-column">
-                                <h2 class="h2 text-uppercase fw-bold">Actualités GTA 6</h2>
+                            <div class="col-12 mt-3 justify-content-center d-flex flex-column align-items-center">
+                                <h2 class="h2 text-uppercase fw-bold">Call of Duty: Modern Warfare 3</h2>
+                                <p class="text-center mt-2">Call of Duty: Modern Warfare 3", sorti en 2011, est un jeu de tir à la première personne développé par Infinity Ward et Sledgehammer Games.
+                                    Il continue l'histoire intense de conflits modernes de ses prédécesseurs, proposant une campagne solo captivante et un mode multijoueur compétitif.
+                                    Le jeu est reconnu pour son action rapide, ses scénarios variés à l'échelle mondiale et ses graphismes impressionnants pour l'époque.</p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <hr class="hr">
+                        </div>
+                        <div class="col-12 p-0 py-3">
+                            <div class="card mb-3 border-0 bg-transparent cardsActus shadow-lg rounded-5">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="/public/assets/img/MWIII-REVEAL-FULL-TOUT.jpg" class="img-fluid object-fit-cover cardsActus rounded-start rounded-3" alt="Call of Duty: Modern Warfare 3">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body px-3 d-flex flex-column justify-content-end">
+                                            <div>
+                                                <span class="badge rounded-pill text-bg-danger p-2 mb-2 text-uppercase">cod : mw 3</span>
+                                            </div>
+                                            <a href="#" class="stretched-link mt-2 h5 text-decoration-none card-title fw-bold">Meilleures armes Zombie Modern Warfare 3 : lesquelles choisir pour survivre ?</a>
+                                            <p class="card-text mt-2">
+                                                Vous souhaitez connaître les meilleures armes pour le mode Zombie de Call of Duty : Modern Warfare 3 ?
+                                                Au lieu de vous proposer une tier list qui n’aurait que très peu d..
+                                            </p>
+                                            <div>
+                                                <hr class="hr">
+                                            </div>
+                                            <small>
+                                                Il y a 20 heures
+                                                <span class="badge rounded-pill mb-1 mx-1 border text-dark fw-semibold"><i class="bi bi-chat-right-dots mx-1 align-middle"></i>5</span>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-3 shadow-lg widthColRightActu mx-4 rounded-5">
+                        <div class="row">
+                            <div class="col-12 d-flex flex-row text-center align-items-center p-3">
+                                <i class="bi bi-grid fs-1 px-2"></i>
+                                <h5 class="text-uppercase fw-bold"><span class="text-danger">les guides :</span> Call of Duty: Modern Warfare 3</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card mt-3 p-0 border-0 bg-transparent">
+                                    <div class="card-img-top ratio ratio-16x9">
+                                        <img src="/public/assets/img/toutes-infos-gta-vi.webp" class="object-fit-cover rounded-3" alt="Sunset Over the Sea" />
+                                    </div>
+                                    <div class="card-body p-0 mt-1">
+                                        <a href="" class="card-text stretchLinkHover fw-bold text-decoration-none text-dark stretched-link">Some quick example text to build on the card title and make up the...</a>
+                                        <div class="card-text mb-3">
+                                            <small class="text-muted">
+                                                25 déc, 18:05
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-3 p-0 border-0 bg-transparent">
+                                    <div class="card-img-top ratio ratio-16x9">
+                                        <img src="/public/assets/img/toutes-infos-gta-vi.webp" class="object-fit-cover rounded-3" alt="Sunset Over the Sea" />
+                                    </div>
+                                    <div class="card-body p-0 mt-1">
+                                        <a href="" class="card-text stretchLinkHover fw-bold text-decoration-none text-dark stretched-link">Some quick example text to build on the card title and make up the...</a>
+                                        <div class="card-text mb-3">
+                                            <small class="text-muted">
+                                                25 déc, 18:05
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card mt-3 p-0 border-0 bg-transparent">
+                                    <div class="card-img-top ratio ratio-16x9">
+                                        <img src="/public/assets/img/toutes-infos-gta-vi.webp" class="object-fit-cover rounded-3" alt="Sunset Over the Sea" />
+                                    </div>
+                                    <div class="card-body p-0 mt-1">
+                                        <a href="" class="card-text stretchLinkHover fw-bold text-decoration-none text-dark stretched-link">Some quick example text to build on the card title and make up the...</a>
+                                        <div class="card-text mb-3">
+                                            <small class="text-muted">
+                                                25 déc, 18:05
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-center mt-3 mb-4">
+                                    <a href="#" class="btn btn-danger w-50 rounded-5 p-1 fw-bold text-uppercase">
+                                        les guides
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
