bf4e4dd51ece53859d550d36d3cfa591696e5c30
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 <picard.boris@gmail.com>
2023-12-31 13:19:56 +0100

    ajout première partie du quiz

--- views/quiz/quiz.php
+++ views/quiz/quiz.php
@@ -22,22 +22,56 @@
         </div>
     </div>
 </section>
-<section>
+<!-- QUIZ -->
+<section class="bg-light">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <div class="imgLeftQuiz p-0">
-                    <div class="opacityBanner d-flex align-items-center justify-content-center h-100">
-                        <h1 class="text-center fw-bold text-light">Dans le jeu "Call of Duty: Modern Warfare", quel personnage est le chef de la faction Task Force 141 ?</h1>
+            <div class="col-md-8 shadow-lg rounded-4 quizBg mt-4 ">
+                <div class="row justify-content-center opacityBanner p-4 rounded-4">
+                    <div class="col-md-12 justify-content-center d-flex flex-column align-items-center">
+                        <h1 class="text-danger display-3 fw-bold">1/15</h1>
+                    </div>
+                    <div class="col-md-12 text-center">
+                        <h5 class="fw-semibold text-light py-4">
+                            15 secondes
+                        </h5>
+                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar w-75 bg-danger"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 shadow-lg p-5 rounded-4 bg-white mt-4">
+                        <h5 class="fw-semibold fs-4  text-center text-dark">
+                            Dans quel jeu Call of Duty la carte multijoueur "Nuketown" a-t-elle été introduite pour la première fois ?
+                        </h5>
+                    </div>
+                    <div class="col-md-12 bg-dark rounded-4 p-4 w-75 mt-4">
+                        <p class="fw-bold fs-5 text-center m-0 text-light">
+                            Call of Duty: World at War
+                        </p>
+                    </div>
+                    <div class="col-md-12 bg-dark rounded-4 p-4 w-75 mt-4">
+                        <p class="fw-bold fs-5 text-center m-0 text-light">
+                            Call of Duty: World at War
+                        </p>
+                    </div>
+                    <div class="col-md-12 bg-dark rounded-4 p-4 w-75 mt-4">
+                        <p class="fw-bold fs-5 text-center m-0 text-light">
+                            Call of Duty: World at War
+                        </p>
+                    </div>
+                    <div class="col-md-12 bg-dark rounded-4 p-4 w-75 mt-4">
+                        <p class="fw-bold fs-5 text-center m-0 text-light">
+                            Call of Duty: World at War
+                        </p>
+                    </div>
+                    <div class="py-5 col-md-12 w-75">
+                        <button type="submit" class="btn btn-danger w-100 rounded-5 p-3 text-uppercase fw-bold">
+                            Valider
+                        </button>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <h2>1/15</h2>
-            </div>
         </div>
-    </div>
-    </div>
     </div>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
