e41b5dd20d3a31e61fa414633789b29e48d910f1
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 <picard.boris@gmail.com>
2024-02-22 14:48:46 +0100

    maj redirection index

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php 
 
-
-include __DIR__ . '/views/templates/header.php';
-include __DIR__ . '/views/templates/navbar.php';
-include __DIR__ . '/views/home.php';
-include __DIR__ . '/views/templates/socials.php';
-include __DIR__ . '/views/templates/footer.php';
+header('location: /controllers/home-ctrl.php');
+die;
